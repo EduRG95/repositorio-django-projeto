421d8375f3bc5afef476410c33eb17dbc973fe46
Eduardo Rorato Guarienti <eduardo.guarienti@gmail.com>
2023-04-18 11:16:11 -0300

    Organizando por categorias

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
 
+from recipes.models import Recipe
 from utils.recipes.factory import make_recipe
 
 
 # Create your views here.
 def home(request):
+    # essa função abaixo chama todas as receitas la do models,
+    # foram importadas e ordenadas agora de tras para frente com o
+    # simbolo - antes do id
+    recipes = Recipe.objects.all().order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(10)],
+        'recipes': recipes,
+    })
+
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(
+        category__id=category_id
+        ).order_by('-id')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes,
     })
 
 
